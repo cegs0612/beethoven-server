70dbfffb70a678ba1bbcf5b377ffc36929836224
CEGS <ce_gs@hotmail.com>
2023-03-16 14:59:28 -0400

    db name added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    //database: 'beethoven',
+    database: process.env.DB_NAME,
 })
 
 db.connect((err)=>{
